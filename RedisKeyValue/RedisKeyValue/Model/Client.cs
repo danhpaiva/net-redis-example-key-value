22bab35257710b4d631d313a1bb8eaf1964824a4
Paiva <danpaiva@live.com>
2022-07-24 20:48:39 -0300

    First commit

---
+++ RedisKeyValue/RedisKeyValue/Model/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace RedisKeyValue.Model
+{
+    public class Client
+    {
+        public string Cpf { get; private set; }
+        public string? Name { get; set; }
+        public string? Document { get; set; }
+
+        public Client(string cpf)
+        {
+            Cpf = cpf;
+        }
+    }
+}
---
+++ RedisKeyValue/RedisKeyValue/Program.cs
@@ -0,0 +1,20 @@
+﻿using RedisKeyValue.Model;
+using ServiceStack.Redis;
+
+var connection = "localhost:6379";
+
+var cpfOne = "12345678910";
+var clientOne = new Client(cpfOne) { Document = "123", Name = "Paiva" };
+
+var cpfTwo = "12345678911";
+var clientTwo = new Client(cpfTwo) { Document = "456", Name = "Raphael" };
+
+using (var redisClient = new RedisClient(connection))
+{
+    redisClient.Set<Client>(clientOne.Cpf.ToString(), clientOne);
+    redisClient.Set<Client>(clientTwo.Cpf, clientTwo);
+
+    var clientX = redisClient.Get<Client>(clientOne.Cpf);
+}
+
+Console.Read();
